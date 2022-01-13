f9482f8b0a38ed248b05e576562b35228eb995d3
blessedjasonmwanza <mwanzabj@gmail.com>
2022-01-13 16:24:12 +0200

    Complete todo Application core functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import TodoContainer from "./components/TodoContainer"
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import TodoContainer from './components/TodoContainer';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router basename={process.env.PUBLIC_URL}>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
-  document.getElementById("root")
-)
+  document.getElementById('root'),
+);
